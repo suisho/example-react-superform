526db1d6913c78349480658e563e030b1c883ef3
suisho <suisho000000@gmail.com>
2015-03-02 21:23:24 +0900

    toggleなボタンブロックを追加

--- js/component/Toggle.jsx
+++ js/component/Toggle.jsx
@@ -1,12 +1,36 @@
+// open / closeなtoggleボタン
 var React = require("react")
 
 module.exports = React.createClass({
   getInitialState(){
     return {
-      open : true
+      open : false
     }
   },
+  buttonMessage(){
+    return this.state.open ? "Close" : "Open"
+  },
+  display(){
+    return this.state.open ? "block" : "none"
+  },
+  onClick(e){
+    e.stopPropagation()
+    e.preventDefault()
+    this.setState({
+      open : !this.state.open
+    })
+  },
   render(){
-    return (<button>Open</button>)
+    var style = {
+      display : this.display()
+    }
+    return (
+      <div>
+        <button onClick={this.onClick}>{this.buttonMessage()}</button>
+        <div style={style}>
+          {this.props.children}
+        </div>
+      </div>
+    )
   }
 })
