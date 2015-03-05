a59aaded7cdfaa08ae93e65de8ae5cf9de2a05b2
suisho <suisho000000@gmail.com>
2015-03-06 00:40:25 +0900

    clean

--- js/view/build_kana_state.js
+++ js/view/build_kana_state.js
@@ -1,25 +1,5 @@
-var kanautil = require("kanautil")
 var japanese = require("japanese")
-var operateDiff = require("../lib/operate_diff")
-var JsDiff = require("diff")
-var extend = require("extend")
 var kanamap = require("../lib/kanamap")
-
-var getActive = function(value){
-  var lastKana = kanautil.split(value).pop() || ""
-  var nextActive = kanautil.isHiragana(lastKana) ? lastKana : ""
-
-  return nextActive || ""
-}
-
-var getSanitizedActive = function(value){
-  var split = kanautil.split(value) || []
-  return split.reverse()[1] || ""
-}
-
-var extendMap = function(map, prev, current){
-  return kanamap(prev, current, map)
-}
 
 var toKana = function(map, value){
   var kana = value
@@ -32,15 +12,6 @@ var toKana = function(map, value){
   return m ? m.join("") : ""
 }
 
-var sanitize = function(kana){
-  var lastSplit = kanautil.split(kana).pop() || ""
-  if(kanautil.isHiragana(lastSplit)){
-    return kana
-  }
-  var reg = new RegExp(lastSplit + "$")
-  return kana.replace(reg, "")
-}
-
 var main = function(state){
   var prev = japanese.hiraganize(state.prev || "")
   var current = japanese.hiraganize(state.value || "")
@@ -48,9 +19,8 @@ var main = function(state){
   if(prev === current){
     return state
   }
-  map = extendMap(map, prev, current)
+  map = kanamap(prev, current, map)
   var kana = toKana(map, current)
-  kana = sanitize(kana, "")
 
   var nextState = {
     kana : kana,
